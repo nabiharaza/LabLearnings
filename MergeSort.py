b5ae8915500e190c195cdfaa263e93bb68e0bdde
nabiharaza <nabiha_137@hotmail.com>
2018-10-15 09:15:09 -0400

    Merge Sort
    Signed-off-by: nabiharaza <nabiha_137@hotmail.com>

--- MergeSort.py
+++ MergeSort.py
@@ -1,12 +1,43 @@
-def MergeSort(list, left, right):
-    middle = (left + right) / 2
-    MergeSort(list, left, middle)
+def mergeSort(alist):
+    print("Splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+    print("Merging ", alist)
+
+
 
 
 def main():
-    list[10, 21, 3, 4, 0, 2]
-    len = len(list)
-    MergeSort(list, list[0], list[len - 1])
+    alist = [10, 21, 3, 4, 0, 2]
+    mergeSort(alist)
+    print alist
 
 
 if __name__ == "__main__":
